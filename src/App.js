36527db958405eef1a900aa1a3b7a7fcdb687a31
Reazour Rahaman Rabbi <alexrabbi111@gmail.com>
2022-04-11 16:49:14 +0600

    ff

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Dashboard from './Pages/Dashboard/Dashboard';
-import Prototype from './Pages/Prototype/Prototype';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Reg from "./Pages/Authentication/Reg";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Prototype from "./Pages/Prototype/Prototype";
 
 function App() {
-
   return (
     <div className="App">
-    <Routes>
-      <Route exact path="/" element={<Prototype></Prototype>}/>
+      <Routes>
+        <Route exact path="/" element={<Prototype></Prototype>} />
+        <Route path="/reg" element={<Reg/>} />
 
-      <Route exact path="/dashboard" element={<Dashboard>
-        <Route exact path="/" element={<Prototype></Prototype>}/>
-      </Dashboard>}/>
-    </Routes>
+        <Route
+          exact
+          path="/dashboard"
+          element={
+            <Dashboard>
+              <Route exact path="/" element={<Prototype></Prototype>} />
+            </Dashboard>
+          }
+        />
+      </Routes>
     </div>
   );
 }
